92f345c1ecb4d93316572b08a34a95c5687e20ab
Lee Yan Hwa <yanhwa.lee@thoughtworks.com>
2019-12-03 11:24:21 +0800

    Prove that exports.add is same as module.exports.add

--- calculate.js
+++ calculate.js
@@ -2,4 +2,3 @@ var math = require("./math.js");
 console.log(math);
 var sum = math.add(1, 2);
 console.log(sum);
-console.log(exports === module.exports);
--- math.js
+++ math.js
@@ -1,3 +1,6 @@
 exports.add = function(number1, number2) {
   return number1 + number2;
 };
+
+console.log(exports === module.exports);
+console.log(exports.add === module.exports.add);
